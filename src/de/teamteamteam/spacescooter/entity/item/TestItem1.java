6b5af72666c58a294dee8d992802c9d010dd3f2d
Licht <johannes.huschke@gmx.de>
2014-11-18 13:14:21 +0100

    Updatet a little change in the Items

--- src/de/teamteamteam/spacescooter/entity/item/TestItem1.java
+++ src/de/teamteamteam/spacescooter/entity/item/TestItem1.java
@@ -6,7 +6,7 @@ public class TestItem1 extends Item {
 		
 	public TestItem1(int x, int y) {
 		super(x, y);
-		this.setImage("images/items/item4.png");
+		this.setImage("images/items/item.png");
 	}
 
 	@Override
