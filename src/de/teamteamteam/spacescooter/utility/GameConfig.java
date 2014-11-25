c8b8c2fd79c1f78f4b214238e448074a02b0f7ac
Jan Philipp Timme <jan.philipp@timme.it>
2014-11-25 14:27:38 +0100

    Disable debug mode. oops.

--- src/de/teamteamteam/spacescooter/utility/GameConfig.java
+++ src/de/teamteamteam/spacescooter/utility/GameConfig.java
@@ -8,7 +8,7 @@ public class GameConfig {
 	/**
 	 * Whether debug output (and more) is enabled or disabled.
 	 */
-	public static boolean DEBUG = true;
+	public static boolean DEBUG = false;
 	
 	/**
 	 * Width of GameWindow.
