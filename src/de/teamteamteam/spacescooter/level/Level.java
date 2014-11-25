d2e28477f0fa0cbacbb80b11fb80ce22ef4255f4
Jan Philipp Timme <jan.philipp@timme.it>
2014-11-25 11:46:23 +0100

    Add spawn-Rules to LevelConfigParser feature set.

--- src/de/teamteamteam/spacescooter/level/Level.java
+++ src/de/teamteamteam/spacescooter/level/Level.java
@@ -9,10 +9,6 @@ import de.teamteamteam.spacescooter.entity.enemy.EnemyBoss;
 import de.teamteamteam.spacescooter.entity.enemy.EnemyFour;
 import de.teamteamteam.spacescooter.entity.enemy.EnemyThree;
 import de.teamteamteam.spacescooter.entity.item.ItemChance;
-import de.teamteamteam.spacescooter.gui.HealthBar;
-import de.teamteamteam.spacescooter.gui.InterfaceBar;
-import de.teamteamteam.spacescooter.gui.ScoreBar;
-import de.teamteamteam.spacescooter.gui.ShieldBar;
 import de.teamteamteam.spacescooter.screen.GameScreen;
 import de.teamteamteam.spacescooter.utility.Loader;
 
@@ -44,6 +40,7 @@ public final class Level {
 	 * Initialize the level based on the LevelConfig attributes.
 	 */
 	public void doBuildUp() {
+		new StarBackground(0, 50);
 		GameScreen.setPlayer(new Player(200, 300));
 		
 		
@@ -53,9 +50,6 @@ public final class Level {
 		points.add(new Point(600,100));
 		points.add(new Point(0,500));
 		new EnemyFour(800, 400, points);
-		
-		new StarBackground(0, 50);
-
 
 		new EnemyThree(450, 100);
 		new EnemyBoss(200, 300);
